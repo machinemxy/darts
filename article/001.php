fa9dc81d5d77843bd03cf214f721fb2191e3fdc7
Cecil Ma <machinemxy@126.com>
2015-01-10 23:42:20 +0900

    1.01
    Correct a mistake in article 001

--- article/001.php
+++ article/001.php
@@ -7,7 +7,7 @@
 <body>
 <h1>How I started playing darts(1)</h1>
 <p>
-	As far as I remember, the first time I played darts when I am a senior middle school student. At that time, I put the board on my board and laid it on the wall. I didn't know the standard height and distance the board should be but just put it there and shooted it for fun.
+	As far as I remember, the first time I played darts when I am a senior middle school student. At that time, I put the board on my bed and laid it on the wall. I didn't know the standard height and distance the board should be but just put it there and shooted it for fun.
 </p>
 <p>
 	In a summer holiday, the local community held a darts contest for young students. I participated in it with two of my friends as a team. The rule was simple. The board was just like an archery target. When you shooted at the bull, you got 10 points. The outer the score was lower. We shooted it one by one and at last the score was summed up. We found it seemed that we are the eldest participators. Other teams were grouped up by junior middles school students or even primary school students. Some of them were too short to reach the bull. So although my team was really not good at darts, we got the second prize at last.
